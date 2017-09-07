f3035bb0eabcf07d9ac6daacc245a5dd30ae6803
jProgr <8537691@gmail.com>
2017-09-06 19:26:17 -0500

    Add dictionary data

--- data/toki_pona_dictionary.js
+++ data/toki_pona_dictionary.js
@@ -1,5 +1,23 @@
 toki_pona_dictionary = [
   {
+    "word": "a",
+    "meanings": [
+      [
+        "interj",
+        "ah! ha! oh! ooh! aw! (emotion word)"
+      ]
+    ]
+  },
+  {
+    "word": "akesi",
+    "meanings": [
+      [
+        "n",
+        "non-cute animal, reptile, amphibian, dinosaur, monster"
+      ]
+    ]
+  },
+  {
     "word": "ala",
     "meanings": [
       [
@@ -13,6 +31,118 @@ toki_pona_dictionary = [
       [
         "interj",
         "no!"
+      ]
+    ]
+  },
+  {
+    "word": "ali (ale)",
+    "meanings": [
+      [
+        "n",
+        "everything, anything, life, the universe"
+      ],
+      [
+        "mod",
+        "all, every, complete, whole"
+      ]
+    ]
+  },
+  {
+    "word": "anpa",
+    "meanings": [
+      [
+        "n",
+        "bottom, down, under, below, floor, beneath"
+      ],
+      [
+        "mod",
+        "low, lower, bottom"
+      ],
+      [
+        "vt",
+        "lower, down, defeat, overcome"
+      ]
+    ]
+  },
+  {
+    "word": "ante",
+    "meanings": [
+      [
+        "n",
+        "difference"
+      ],
+      [
+        "mod",
+        "different"
+      ],
+      [
+        "cont",
+        "otherwise, or else"
+      ],
+      [
+        "vt",
+        "change, alter, modify"
+      ]
+    ]
+  },
+  {
+    "word": "anu",
+    "meanings": [
+      [
+        "conj",
+        "or"
+      ]
+    ]
+  },
+  {
+    "word": "awen",
+    "meanings": [
+      [
+        "vi",
+        "stay, wait, remain"
+      ],
+      [
+        "vt",
+        "keep"
+      ],
+      [
+        "mod",
+        "remaining, stationary, permanent, sedentary"
+      ]
+    ]
+  },
+  {
+    "word": "e",
+    "meanings": [
+      [
+        "sep",
+        "(introduces a direct object)"
+      ]
+    ]
+  },
+  {
+    "word": "en",
+    "meanings": [
+      [
+        "conj",
+        "and (used to coordinate head nouns)"
+      ]
+    ]
+  },
+  {
+    "word": "ijo",
+    "meanings": [
+      [
+        "n",
+        "thing, something, stuff, anything, object"
+      ],
+      [
+        "mod",
+        "of something"
+      ],
+      [
+        "vt",
+        "objectify"
       ]
     ]
   },
@@ -40,5 +170,1077 @@ toki_pona_dictionary = [
         "to be bad, to suck"
       ]
     ]
+  },
+  {
+    "word": "ilo",
+    "meanings": [
+      [
+        "n",
+        "tool, device, machine, thing used for a specific purpose"
+      ]
+    ]
+  },
+  {
+    "word": "insa",
+    "meanings": [
+      [
+        "n",
+        "inside, inner world, centre, stomach"
+      ],
+      [
+        "mod",
+        "internal, central"
+      ]
+    ]
+  },
+  {
+    "word": "jaki",
+    "meanings": [
+      [
+        "mod",
+        "dirty, gross, filthy"
+      ],
+      [
+        "n",
+        "dirt, pollution, filth"
+      ],
+      [
+        "vt",
+        "pollute, dirty"
+      ],
+      [
+        "interj",
+        "ew! yuck!"
+      ]
+    ]
+  },
+  { 
+    "word": "jan",
+    "meanings": [
+      [
+        "n",
+        "person, people, human, being, somebody, anybody"
+      ],
+      [
+        "mod",
+        "personal, human, somebody's, of people"
+      ],
+      [
+        "vt",
+        "personify, humanize, personalize"
+      ]
+    ]
+  },
+  { 
+    "word": "jelo",
+    "meanings": [
+      [
+        "mod",
+        "yellow, light green"
+      ]
+    ]
+  },
+  {
+    "word":"jo",
+    "meanings":[
+      [
+        "vt",
+        "have, contain"
+      ],
+      [
+        "n",
+        "having"
+      ],
+      [
+        "kama",
+        "receive, get, take, obtain"
+      ]
+    ]
+  },
+  {
+    "word":"kala",
+    "meanings":[
+      [
+        "n",
+        "fish, sea creature"
+      ]
+    ]
+  },
+  {
+    "word":"kalama",
+    "meanings":[
+      [
+        "n",
+        "sound, noise, voice"
+      ],
+      [
+        "vi",
+        "make noise"
+      ],
+      [
+        "vt",
+        "sound, ring, play (an instrument)"
+      ]
+    ]
+  },
+  {
+    "word":"kama",
+    "meanings":[
+      [
+        "n",
+        "event, happening, chance, arrival, beginning"
+      ],
+      [
+        "mod",
+        "coming, future"
+      ],
+      [
+        "vi",
+        "come, become, arrive, happen, pursue actions to arrive to (a certain state), manage to, start to"
+      ],
+      [
+        "vt",
+        "bring about, summon"
+      ]
+    ]
+  },
+  {
+    "word":"kasi",
+    "meanings":[
+      [
+        "n",
+        "plant, leaf, herb, tree, wood"
+      ]
+    ]
+  },
+  {
+    "word":"ken",
+    "meanings":[
+      [
+        "n",
+        "possibility, ability, power to do things, permission"
+      ],
+      [
+        "vi",
+        "can, is able to, is allowed to, may, is possible"
+      ],
+      [
+        "vt",
+        "make possible, enable, allow, permit"
+      ],
+      [
+        "cont",
+        "it is possible that"
+      ]
+    ]
+  },
+  {
+    "word":"kepeken",
+    "meanings":[
+      [
+        "vt",
+        "use"
+      ],
+      [
+        "prep",
+        "with"
+      ]
+    ]
+  },
+  {
+    "word":"kili",
+    "meanings":[
+      [
+        "n",
+        "fruit, pulpy vegetable, mushroom"
+      ]
+    ]
+  },
+  {
+    "word":"kin",
+    "meanings":[
+      [
+        "mod",
+        "also, too, even, indeed (emphasizes the word that follows)"
+      ]
+    ]
+  },
+  {
+    "word":"kiwen",
+    "meanings":[
+      [
+        "n",
+        "hard thing, rock, stone, metal, mineral, clay"
+      ],
+      [
+        "mod",
+        "hard, solid, stone-like, made of stone or metal"
+      ]
+    ]
+  },
+  {
+    "word":"ko",
+    "meanings":[
+      [
+        "mod",
+        "air-like, ethereal, gaseous"
+      ]
+    ]
+  },
+  {
+    "word":"kon",
+    "meanings":[
+      [
+        "n",
+        "air, wind, odour, soul"
+      ],
+      [
+        "mod",
+        "air-like, ethereal, gaseous"
+      ]
+    ]
+  },
+  {
+    "word":"kule",
+    "meanings":[
+      [
+        "n",
+        "color, paint"
+      ],
+      [
+        "vt",
+        "color, paint"
+      ],
+      [
+        "mod",
+        "colourful"
+      ]
+    ]
+  },
+  {
+    "word":"kulupu",
+    "meanings":[
+      [
+        "mod",
+        "communal, shared, public, of the society"
+      ]
+    ]
+  },
+  {
+    "word":"kute",
+    "meanings":[
+      [
+        "vt",
+        "listen, hear"
+      ],
+      [
+        "mod",
+        "auditory, hearing"
+      ]
+    ]
+  },
+  {
+    "word":"la",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"lape",
+    "meanings":[
+      [
+        "mod",
+        "sleeping, of sleep laso mod blue, blue-green"
+      ]
+    ]
+  },
+  {
+    "word":"lawa",
+    "meanings":[
+      [
+        "mod",
+        "main, leading, in charge"
+      ],
+      [
+        "vt",
+        "lead, control, rule, steer len"
+      ],
+      [
+        "n",
+        "clothing, cloth, fabric"
+      ]
+    ]
+  },
+  {
+    "word":"lete",
+    "meanings":[
+      [
+        "mod",
+        "cold, uncooked"
+      ],
+      [
+        "vt",
+        "cool down, chill, freeze"
+      ]
+    ]
+  },
+  {
+    "word":"li",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"lili",
+    "meanings":[
+      [
+        "vt",
+        "reduce, shorten, shrink, lessen"
+      ]
+    ]
+  },
+  {
+    "word":"linja",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"lipu",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"loje",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"lon",
+    "meanings":[
+      [
+        "vi",
+        "be there, be present, be real/true, exist, be awake"
+      ]
+    ]
+  },
+  {
+    "word":"luka",
+    "meanings":[
+      [
+        "mod",
+        "five"
+      ]
+    ]
+  },
+  {
+    "word":"lukin",
+    "meanings":[
+      [
+        "vi",
+        "look, watch out, pay attention"
+      ],
+      [
+        "mod",
+        "visual"
+      ]
+    ]
+  },
+  {
+    "word":"lupa",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"ma",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"mama",
+    "meanings":[
+      [
+        "mod",
+        "of the parent, parental, maternal, fatherly"
+      ]
+    ]
+  },
+  {
+    "word":"mani",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"meli",
+    "meanings":[
+      [
+        "mod",
+        "female, feminine, womanly"
+      ]
+    ]
+  },
+  {
+    "word":"mi",
+    "meanings":[
+      [
+        "mod",
+        "my, our"
+      ]
+    ]
+  },
+  {
+    "word":"mije",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"moku",
+    "meanings":[
+      [
+        "vt",
+        "eat, drink, swallow, ingest, consume"
+      ]
+    ]
+  },
+  {
+    "word":"moli",
+    "meanings":[
+      [
+        "mod",
+        "dead, deadly, fatal"
+      ],
+      [
+        "vt",
+        "kill"
+      ],
+      [
+        "vi",
+        "die, be dead"
+      ]
+    ]
+  },
+  {
+    "word":"monsi",
+    "meanings":[
+      [
+        "mod",
+        "back, rear"
+      ]
+    ]
+  },
+  {
+    "word":"mu",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"mun",
+    "meanings":[
+      [
+        "mod",
+        "lunar"
+      ]
+    ]
+  },
+  {
+    "word":"musi",
+    "meanings":[
+      [
+        "vi",
+        "play, have fun"
+      ],
+      [
+        "vt",
+        "amuse, entertain"
+      ]
+    ]
+  },
+  {
+    "word":"mute",
+    "meanings":[
+      [
+        "n",
+        "amount, quantity"
+      ],
+      [
+        "vt",
+        "make many or much"
+      ]
+    ]
+  },
+  {
+    "word":"nanpa",
+    "meanings":[
+      [
+        "oth",
+        "-th (ordinal numbers)"
+      ]
+    ]
+  },
+  {
+    "word":"nasa",
+    "meanings":[
+      [
+        "vt",
+        "drive crazy, make weird"
+      ]
+    ]
+  },
+  {
+    "word":"nasin",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"nena",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"ni",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"nimi",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"noka",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"o",
+    "meanings":[
+      [
+        "interj",
+        "hey! (calling somebody's attention)"
+      ]
+    ]
+  },
+  {
+    "word":"oko",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"olin",
+    "meanings":[
+      [
+        "mod",
+        "love"
+      ],
+      [
+        "vt",
+        "to love (a person)"
+      ]
+    ]
+  },
+  {
+    "word":"ona",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"open",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"pakala",
+    "meanings":[
+      [
+        "vt",
+        "screw up, fuck up, botch, ruin, break, hurt, injure, damage, bungle, spoil, ruin"
+      ],
+      [
+        "vi",
+        "screw up, fall apart, break"
+      ],
+      [
+        "interj",
+        "damn! fuck!"
+      ]
+    ]
+  },
+  {
+    "word":"pali",
+    "meanings":[
+      [
+        "mod",
+        "active, work-related, operating, working"
+      ],
+      [
+        "vt",
+        "do, make, build, create"
+      ],
+      [
+        "vi",
+        "act, work, function"
+      ]
+    ]
+  },
+  {
+    "word":"palisa",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"pana",
+    "meanings":[
+      [
+        "n",
+        "giving, transfer, exchange"
+      ]
+    ]
+  },
+  {
+    "word":"pi",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"pilin",
+    "meanings":[
+      [
+        "vi",
+        "feel"
+      ],
+      [
+        "vt",
+        "feel, think, sense, touch"
+      ]
+    ]
+  },
+  {
+    "word":"pimeja",
+    "meanings":[
+      [
+        "n",
+        "darkness, shadows"
+      ],
+      [
+        "vt",
+        "darken"
+      ]
+    ]
+  },
+  {
+    "word":"pini",
+    "meanings":[
+      [
+        "mod",
+        "completed, finished, past, done, ago"
+      ],
+      [
+        "vt",
+        "finish, close, end, turn off"
+      ]
+    ]
+  },
+  {
+    "word":"pipi",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"poka",
+    "meanings":[
+      [
+        "mod",
+        "neighboring"
+      ],
+      [
+        "prep",
+        "in the accompaniment of, with"
+      ]
+    ]
+  },
+  {
+    "word":"poki",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"pona",
+    "meanings":[
+      [
+        "mod",
+        "good, simple, positive, nice, correct, right"
+      ],
+      [
+        "interj",
+        "great! good! thanks! OK! cool! yay!"
+      ],
+      [
+        "vt",
+        "improve, fix, repair, make good"
+      ]
+    ]
+  },
+  {
+    "word":"sama",
+    "meanings":[
+      [
+        "prep",
+        "like, as, seem"
+      ],
+      [
+        "cont",
+        "similarly, in the same way that"
+      ]
+    ]
+  },
+  {
+    "word":"seli",
+    "meanings":[
+      [
+        "mod",
+        "hot, warm, cooked"
+      ],
+      [
+        "vt",
+        "heat, warm up, cook"
+      ]
+    ]
+  },
+  {
+    "word":"selo",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"seme",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"sewi",
+    "meanings":[
+      [
+        "mod",
+        "superior, elevated, religious, formal"
+      ]
+    ]
+  },
+  {
+    "word":"sijelo",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"sike",
+    "meanings":[
+      [
+        "mod",
+        "round, cyclical"
+      ]
+    ]
+  },
+  {
+    "word":"sin",
+    "meanings":[
+      [
+        "vt",
+        "renew, renovate, freshen"
+      ]
+    ]
+  },
+  {
+    "word":"sina",
+    "meanings":[
+      [
+        "mod",
+        "your"
+      ]
+    ]
+  },
+  {
+    "word":"sinpin",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"sitelen",
+    "meanings":[
+      [
+        "vt",
+        "draw, write"
+      ]
+    ]
+  },
+  {
+    "word":"sona",
+    "meanings":[
+      [
+        "vt",
+        "know, understand, know how to"
+      ],
+      [
+        "vi",
+        "know, understand"
+      ],
+      [
+        "kama",
+        "learn, study"
+      ]
+    ]
+  },
+  {
+    "word":"soweli",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"suli",
+    "meanings":[
+      [
+        "vt",
+        "enlarge, lengthen"
+      ],
+      [
+        "n",
+        "size"
+      ]
+    ]
+  },
+  {
+    "word":"suno",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"supa",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"suwi",
+    "meanings":[
+      [
+        "mod",
+        "sweet, cute"
+      ],
+      [
+        "vt",
+        "sweeten"
+      ]
+    ]
+  },
+  {
+    "word":"tan",
+    "meanings":[
+      [
+        "n",
+        "origin, cause"
+      ]
+    ]
+  },
+  {
+    "word":"taso",
+    "meanings":[
+      [
+        "conj",
+        "but"
+      ]
+    ]
+  },
+  {
+    "word":"tawa",
+    "meanings":[
+      [
+        "mod",
+        "moving, mobile"
+      ],
+      [
+        "vi",
+        "go, leave, walk, travel, move"
+      ],
+      [
+        "vt",
+        "move, displace"
+      ]
+    ]
+  },
+  {
+    "word":"telo",
+    "meanings":[
+      [
+        "vt",
+        "water, wash with water"
+      ]
+    ]
+  },
+  {
+    "word":"tenpo",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"toki",
+    "meanings":[
+      [
+        "vt",
+        "say"
+      ],
+      [
+        "vi",
+        "talk, chat, communicate"
+      ],
+      [
+        "interj",
+        "hello! hi!"
+      ]
+    ]
+  },
+  {
+    "word":"tomo",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"tu",
+    "meanings":[
+      [
+        "n",
+        "duo, pair"
+      ],
+      [
+        "vt",
+        "double, separate/cut/divide in two"
+      ]
+    ]
+  },
+  {
+    "word":"unpa",
+    "meanings":[
+      [
+        "mod",
+        "erotic, sexual"
+      ],
+      [
+        "vt",
+        "have sex with, sleep with, fuck"
+      ],
+      [
+        "vi",
+        "have sex"
+      ]
+    ]
+  },
+  {
+    "word":"uta",
+    "meanings":[
+      [
+        "mod",
+        "oral"
+      ]
+    ]
+  },
+  {
+    "word":"utala",
+    "meanings":[
+      [
+        "vt",
+        "hit, strike, attack, compete against walo"
+      ],
+      [
+        "mod",
+        "white, light (colour)"
+      ],
+      [
+        "n",
+        "white thing/part, whiteness, lightness"
+      ]
+    ]
+  },
+  {
+    "word":"wan",
+    "meanings":[
+      [
+        "n",
+        "unit, element, particle, part, piece"
+      ],
+      [
+        "vt",
+        "unite, make one"
+      ]
+    ]
+  },
+  {
+    "word":"waso",
+    "meanings":[
+
+    ]
+  },
+  {
+    "word":"wawa",
+    "meanings":[
+      [
+        "mod",
+        "energetic, strong, fierce, intense, sure, confident"
+      ],
+      [
+        "vt",
+        "strengthen, energize, empower"
+      ]
+    ]
+  },
+  {
+    "word":"weka",
+    "meanings":[
+      [
+        "n",
+        "absence"
+      ],
+      [
+        "vt",
+        "throw away, remove, get rid of"
+      ]
+    ]
+  },
+  {
+    "word":"wile",
+    "meanings":[
+      [
+        "n",
+        "desire, need, will"
+      ],
+      [
+        "mod",
+        "necessary"
+      ]
+    ]
   }
+  
 ];
