f45303456c852d4b3b2bb050a467f282dd2e88fc
mitaa <mitaa.ceb@gmail.com>
2021-01-21 02:54:25 +0100

    Improve how search results are shown
    Show whole word matches highlighted at the top and substring matches below those.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import './styles/animations.css';
 import './styles/main.css';
 import {ElementSearcher} from './search';
 
-const wordElements = document.querySelectorAll('div[class=word]');
-const searcher = new ElementSearcher(wordElements);
+const wordElements = document.querySelectorAll('div[class=entry]');
+const exactResults = document.querySelector('div[id=exact-results]');
+const searcher = new ElementSearcher(exactResults, wordElements);
 
 const searchElement = document.getElementById('search');
 searchElement.addEventListener('keyup', () => {
