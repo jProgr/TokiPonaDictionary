18a8d41d639533734017a4e6ff7802981a5546a3
kmaasrud <km@aasrud.com>
2021-07-22 11:11:59 +0200

    Hide compounds when not searching

--- src/main.js
+++ src/main.js
@@ -2,11 +2,33 @@ import './styles/animations.css';
 import './styles/main.css';
 import {ElementSearcher} from './search';
 
+var compounds = document.getElementsByClassName('compound');
 const wordElements = document.querySelectorAll('div[class=word]');
 const searcher = new ElementSearcher(wordElements);
 
+function hideCompounds() {
+    for (var i = 0; i < compounds.length; i++) {
+      compounds[i].parentElement.classList.add("hidden");
+    }
+}
+
+function showCompounds() {
+    for (var i = 0; i < compounds.length; i++) {
+      compounds[i].parentElement.classList.remove("hidden");
+    }
+}
+
+hideCompounds();
 const searchElement = document.getElementById('search');
 searchElement.addEventListener('keyup', () => {
   const query = searchElement.value.trim().toLowerCase();
+
+  console.log(query.length)
+  if (query.length == 0) {
+    hideCompounds();
+  } else {
+    showCompounds();
+  }
+
   searcher.search(query);
 });
